8410200e056add09aec8e3530ce6f61da792be89
Anna Shevchenko <uaskoa@gmail.com>
2021-03-16 22:50:31 +0200

    v alfa

--- src/index.js
+++ src/index.js
@@ -1,22 +1,127 @@
 import './styles.css';
 import debounce from 'lodash';
-import ImagesApi from './js/apiService';
 import 'material-design-icons/iconfont/material-icons.css';
+import ImgApiService from './js/apiService';
+import galleryTemplate from './templates/gallery-items.hbs';
 
-const imagesSearch = new ImagesApi('.gallery');
-const inputRef = document.querySelector('input[name="query"]');
-const observerRef = document.querySelector('.gallery-load-observer');
+
+const inputRef = document.querySelector('.search-form input');
+const loadMoreButtonRef = document.querySelector('.load-more');
+const galleryRef = document.querySelector('.gallery');
+const modalRef = document.querySelector('.js-lightbox');
+const modalImgRef = document.querySelector('.lightbox-image');
+const closeButtonRef = document.querySelector(
+  'button[data-action="close-lightbox"]',
+);
+const overlayRef = document.querySelector('.lightbox-overlay');
+
+console.log(galleryRef);
+
+const imgApiServie = new ImgApiService();
 
 inputRef.addEventListener(
   'input',
   _.debounce(() => {
-    imagesSearch.search(inputRef.value);
+    onSearch();
   }, 1000),
 );
+loadMoreButtonRef.addEventListener('click', onLoadMore);
+
+galleryRef.addEventListener('click', openModal);
+closeButtonRef.addEventListener('click', closeModal);
+overlayRef.addEventListener('click', closeModal);
 
 
+function onSearch() {
+
+  if (!inputRef.value) {
+     return;
+  }
+  clearImg();
+  imgApiServie.query = inputRef.value;
+  imgApiServie.resetPage();
+
+  imgApiServie
+    .fetchImg()
+    .then(renderImg)
+    
+  
+
+}
+
+function onLoadMore() {
+  imgApiServie.fetchImg().then(renderImg);
+  scrollAfterRender()
+}
+
+function renderImg(hits) {
+  if (hits.length !== 0) {
+      setTimeout(() => {
+    showLoadMoreBtn();
+  }, 500);
+    
+  }
+  galleryRef.insertAdjacentHTML('beforeend', galleryTemplate(hits));
+  console.log();
+}
+
+function clearImg() {
+  galleryRef.innerHTML = '';
+}
+
+function showLoadMoreBtn() {
+
+   
+  loadMoreButtonRef.classList.remove('is-hidden');
+}
+
+function scrollAfterRender() {
+  console.log('start scrool');
+
+setTimeout(() => { window.scrollTo({
+  top: window.scrollY + window.screen.height,
+  behavior: 'smooth',
+});
+  
+}, 500);
 
 
+}
+
+function openModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  modalImgRef.setAttribute('src', event.target.dataset.source);
+  modalImgRef.setAttribute('alt', event.target.alt);
+  
+
+  modalRef.classList.add('is-open');
+}
+
+function closeModal() {
+  modalImgRef.setAttribute('src', '');
+  modalImgRef.setAttribute('alt', '');
+  modalRef.classList.remove('is-open');
+}
+
+
+// Kyryl
+
+// import ImagesApi from './js/apiService';
+// import 'material-design-icons/iconfont/material-icons.css';
+
+// const imagesSearch = new ImagesApi('.gallery');
+// const inputRef = document.querySelector('input[name="query"]');
+// const observerRef = document.querySelector('.gallery-load-observer');
+
+// inputRef.addEventListener(
+//   'input',
+//   _.debounce(() => {
+//     imagesSearch.search(inputRef.value);
+//   }, 1000),
+// );
 
 // const observerHandler = _.debounce(() => {
 //   console.log('Сработал Обс');
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,132 +1,173 @@
-import setting from './settings';
-import imagesListTemplate from '../templates/imagesListTemplate.hbs';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '20667808-d6e3a4866a107921c5b89b931';
 
-const { BASE_URL, API_KEY } = setting;
-
-class ImagesApi {
-  constructor(selector) {
-    this.galleryRef = document.querySelector(selector);
-    this.lightboxRef = document.querySelector('.js-lightbox');
-    this.lightboxImageRef = document.querySelector('.lightbox__image');
-    this.lightboxOverlayRef = document.querySelector('.lightbox__overlay');
-    this.lightboxCloseRef = document.querySelector(
-      'button[data-action="close-lightbox"]',
-    );
-    this.moreButtonRef = document.querySelector('.more');
-    this.currentPage = 1;
-    this.totalPages = 0;
-    this.query = '';
-    this.per_page = 12;
-    this.loadMore = this.loadMore.bind(this);
+export default class ImgApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
   }
 
-  onGalleryClick(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-      return;
-    }
-    console.log('Click on gallery');
-    const imageRef = event.target;
-    const largeImageURL = imageRef.dataset.source;
-    const largeImageALT = imageRef.alt;
-    console.log(largeImageURL);
-    this.setLightboxImage(largeImageURL, largeImageALT);
-    this.openModal();
+  fetchImg() {
+    const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+   // const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=${API_KEY}`;
+
+    // const searchQuery = e.target.value
+
+    
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.increamentPage();
+        console.log(data.hits);
+        return data.hits;
+      });
   }
 
-  openModal() {
-    this.lightboxRef.classList.add('is-open');
+  increamentPage() {
+    this.page += 1;
   }
 
-  closeModal() {
-    this.lightboxRef.classList.remove('is-open');
-    this.removeLightboxImage();
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
   }
 
-  setLightboxImage(url, alt) {
-    this.lightboxImageRef.src = url;
-    this.lightboxImageRef.alt = alt;
-  }
-
-  removeLightboxImage() {
-    this.lightboxImageRef.src = '';
-    this.lightboxImageRef.alt = '';
-  }
-
-  search(query) {
-    if (!query) {
-      return; //no query
-    }
-
-    if (this.query != query) {
-      //new quert = reset contants
-      this.query = query;
-      this.currentPage = 1;
-      this.totalPages = 0;
-      this.galleryRef.innerHTML = '';
-    }
-    console.log(`NEW QUERY`);
-    console.log(`Load Page: ${this.currentPage}`);
-
-    this.fetchImages(query, this.currentPage).then(results => {
-      this.renderImages(results.hits);
-      this.totalPages = results.totalHits / this.per_page;
-      if (results.totalHits / this.per_page > this.currentPage) {
-        this.renderLoadMoreButton();
-      }
-      // console.log(results.hits);
-    });
-    // render lightbox stuff
-    this.galleryRef.addEventListener('click', this.onGalleryClick.bind(this));
-    this.lightboxOverlayRef.addEventListener(
-      'click',
-      this.closeModal.bind(this),
-    );
-    this.lightboxCloseRef.addEventListener('click', this.closeModal.bind(this));
-  }
-
-  loadMore() {
-    if (!this.query) {
-      return;
-    }
-
-    if (this.totalPages <= this.currentPage) {
-      return;
-    }
-
-    this.currentPage += 1;
-    console.log(`Load Page: ${this.currentPage}`);
-
-    this.fetchImages(this.query, this.currentPage).then(results => {
-      this.renderImages(results.hits);
-    });
-    this.scrollAfterRender();
-  }
-
-  renderLoadMoreButton() {
-    this.moreButtonRef.innerHTML =
-      '<button id="load-more" class="load-more">LOAD MORE</button>';
-    const loadMoreBtnRef = document.querySelector('#load-more');
-    loadMoreBtnRef.addEventListener('click', this.loadMore);
-  }
-
-  async fetchImages(searchQuery, page) {
-    const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=${this.per_page}&key=${API_KEY}`;
-    const response = await fetch(url);
-    const images = response.json();
-    return images;
-  }
-
-  renderImages(images) {
-    this.galleryRef.insertAdjacentHTML('beforeend', imagesListTemplate(images));
-  }
-
-  scrollAfterRender() {
-    window.scrollTo({
-      top: window.scrollY + window.screen.height,
-      behavior: 'smooth',
-    });
+  set query(newQuery) {
+    this.searchQuery = newQuery;
   }
 }
 
-export default ImagesApi;
+// import setting from './settings';
+// import imagesListTemplate from '../templates/imagesListTemplate.hbs';
+
+// const { BASE_URL, API_KEY } = setting;
+
+// class ImagesApi {
+//   constructor(selector) {
+//     this.galleryRef = document.querySelector(selector);
+//     this.lightboxRef = document.querySelector('.js-lightbox');
+//     this.lightboxImageRef = document.querySelector('.lightbox__image');
+//     this.lightboxOverlayRef = document.querySelector('.lightbox__overlay');
+//     this.lightboxCloseRef = document.querySelector(
+//       'button[data-action="close-lightbox"]',
+//     );
+//     this.moreButtonRef = document.querySelector('.more');
+//     this.currentPage = 1;
+//     this.totalPages = 0;
+//     this.query = '';
+//     this.per_page = 12;
+//     this.loadMore = this.loadMore.bind(this);
+//   }
+
+//   onGalleryClick(event) {
+//     event.preventDefault();
+//     if (event.target.nodeName !== 'IMG') {
+//       return;
+//     }
+//     console.log('Click on gallery');
+//     const imageRef = event.target;
+//     const largeImageURL = imageRef.dataset.source;
+//     const largeImageALT = imageRef.alt;
+//     console.log(largeImageURL);
+//     this.setLightboxImage(largeImageURL, largeImageALT);
+//     this.openModal();
+//   }
+
+//   openModal() {
+//     this.lightboxRef.classList.add('is-open');
+//   }
+
+//   closeModal() {
+//     this.lightboxRef.classList.remove('is-open');
+//     this.removeLightboxImage();
+//   }
+
+//   setLightboxImage(url, alt) {
+//     this.lightboxImageRef.src = url;
+//     this.lightboxImageRef.alt = alt;
+//   }
+
+//   removeLightboxImage() {
+//     this.lightboxImageRef.src = '';
+//     this.lightboxImageRef.alt = '';
+//   }
+
+//   search(query) {
+//     if (!query) {
+//       return; //no query
+//     }
+
+//     if (this.query != query) {
+//       //new quert = reset contants
+//       this.query = query;
+//       this.currentPage = 1;
+//       this.totalPages = 0;
+//       this.galleryRef.innerHTML = '';
+//     }
+//     console.log(`NEW QUERY`);
+//     console.log(`Load Page: ${this.currentPage}`);
+
+//     this.fetchImages(query, this.currentPage).then(results => {
+//       this.renderImages(results.hits);
+//       this.totalPages = results.totalHits / this.per_page;
+//       if (results.totalHits / this.per_page > this.currentPage) {
+//         this.renderLoadMoreButton();
+//       }
+//       // console.log(results.hits);
+//     });
+//     // render lightbox stuff
+//     this.galleryRef.addEventListener('click', this.onGalleryClick.bind(this));
+//     this.lightboxOverlayRef.addEventListener(
+//       'click',
+//       this.closeModal.bind(this),
+//     );
+//     this.lightboxCloseRef.addEventListener('click', this.closeModal.bind(this));
+//   }
+
+//   loadMore() {
+//     if (!this.query) {
+//       return;
+//     }
+
+//     if (this.totalPages <= this.currentPage) {
+//       return;
+//     }
+
+//     this.currentPage += 1;
+//     console.log(`Load Page: ${this.currentPage}`);
+
+//     this.fetchImages(this.query, this.currentPage).then(results => {
+//       this.renderImages(results.hits);
+//     });
+//     this.scrollAfterRender();
+//   }
+
+//   renderLoadMoreButton() {
+//     this.moreButtonRef.innerHTML =
+//       '<button id="load-more" class="load-more">LOAD MORE</button>';
+//     const loadMoreBtnRef = document.querySelector('#load-more');
+//     loadMoreBtnRef.addEventListener('click', this.loadMore);
+//   }
+
+//   async fetchImages(searchQuery, page) {
+//     const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=${this.per_page}&key=${API_KEY}`;
+//     const response = await fetch(url);
+//     const images = response.json();
+//     return images;
+//   }
+
+//   renderImages(images) {
+//     this.galleryRef.insertAdjacentHTML('beforeend', imagesListTemplate(images));
+//   }
+
+//   scrollAfterRender() {
+//     window.scrollTo({
+//       top: window.scrollY + window.screen.height,
+//       behavior: 'smooth',
+//     });
+//   }
+// }
+
+// export default ImagesApi;
